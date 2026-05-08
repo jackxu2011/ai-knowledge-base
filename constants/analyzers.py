c686fe0d6fcf6b954150b7f0bd37646ca4b074f6
Jack Xu <xulingcom@126.com>
2026-05-08 14:27:44 +0800

    refactor: refactor the data format

---
+++ constants/analyzers.py
@@ -0,0 +1,9 @@
+SCORING_DIMENSIONS = ["tech_depth", "innovation", "usability"]
+
+SCORING_WEIGHTS = {
+    "tech_depth": 0.4,
+    "innovation": 0.35,
+    "usability": 0.25,
+}
+
+DEFAULT_MIN_SCORE = 6.0
